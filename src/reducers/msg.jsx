962ed36c04ae78dbf2722887f20be9dd226e6e2f
Tonypai <tony77794@gmail.com>
2015-12-08 18:34:07 +0800

    Fix message list update issue

--- src/reducers/msg.jsx
+++ src/reducers/msg.jsx
@@ -1,10 +1,10 @@
+import update from "react-addons-update";
 import { GET_MESSAGE_LIST } from "../actions/msg";
 
 export const messageList = (state = [], action) => {
     switch(action.type) {
         case GET_MESSAGE_LIST:
-            return action.messageList;
-           // return action.messageList.filter(msg => state.indexOf(msg) === -1).concat(state)
+            return update(state, {$set: action.messageList});
         default:
             return state;
     }
