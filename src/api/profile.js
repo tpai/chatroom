610e6ca391c8d1e08f9c6102fa8897b4be838fe6
Tony Pai <tony77794@gmail.com>
2019-04-22 23:24:06 +0800

    Use fake data instead external API
    Sometimes API is not working

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,3 +1,16 @@
-import { get } from 'utils/fetch';
-
-export const getProfile = () => get('https://jsonplaceholder.typicode.com/users');
+export const getProfile = () => Promise.resolve({
+  success: {
+    body: [
+      { id: 1, name: 'Ada' },
+      { id: 2, name: 'Billy' },
+      { id: 3, name: 'Chris' },
+      { id: 4, name: 'Derek' },
+      { id: 5, name: 'Eve' },
+      { id: 6, name: 'Faustine' },
+      { id: 7, name: 'George' },
+      { id: 8, name: 'Huffman' },
+      { id: 9, name: 'Isla' },
+      { id: 10, name: 'Jeff' },
+    ],
+  },
+});
